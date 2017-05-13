99e1fc08bd3a115b51464a1b4ac72f1f36e30087
makeiich <mak.hmc@gmail.com>
2017-05-13 18:37:16 +0200

    Template and TranslateExtension

--- Extensions/TranslateExtension.php
+++ Extensions/TranslateExtension.php
@@ -7,11 +7,19 @@ class TranslateExtension extends \Twig_Extension
 {
     private $dataTranslate = [];
 
+    /**
+     * TranslateExtension constructor.
+     * @param string $filename
+     */
     public function __construct(string $filename)
     {
         $this->dataTranslate = $this->parseFile($filename);
     }
 
+    /**
+     * Get filters
+     * @return array
+     */
     public function getFilters()
     {
         return [
@@ -19,14 +27,22 @@ class TranslateExtension extends \Twig_Extension
         ];
     }
 
-
+    /**
+     * Parse file
+     * @param string $filename
+     * @return array
+     */
     private function parseFile(string $filename) : array
     {
         if(file_exists($filename)) return parse_ini_file($filename);
         return [];
     }
 
-
+    /**
+     * Translate filter
+     * @param string $text
+     * @return string
+     */
     public function translateFilter(string $text) : string
     {
         return isset($this->dataTranslate[$text]) ? $this->dataTranslate[$text] : $text;
--- Template.php
+++ Template.php
@@ -15,25 +15,58 @@ class Template
 
     private $pathTemplates = '';
 
-
+    /**
+     * Template constructor.
+     * @param string $pathTemplates
+     * @param string $lang
+     */
     public function __construct(string $pathTemplates, $lang = 'es')
     {
         $this->pathTemplates = $pathTemplates;
         $this->lang = $lang;
     }
 
+    /**
+     * Register languages
+     * @param array $languages
+     * @return void
+     */
     public function registerLanguages(array $languages) : void
     {
         $this->languages += $languages;
     }
 
+    /**
+     * Register language
+     * @param string $language
+     * @param string $filename
+     * @return void
+     */
     public function registerLanguage(string $language, string $filename) : void
     {
         $this->languages[$language] = $filename;
     }
 
+    /**
+     * Set Data
+     * @param string $key
+     * @param mixed $value
+     * @return void
+     */
+    public function setData(string $key, $value) : void
+    {
+        $this->data[$key] = $value;
+    }
+
+    /**
+     * Render
+     * @param string $template
+     * @param array $data
+     * @return string
+     */
     public function render(string $template, $data = []) : string
     {
+        $this->data += $data;
 
         $loader = new \Twig_Loader_Filesystem($this->pathTemplates);
         $twig = new \Twig_Environment($loader);
